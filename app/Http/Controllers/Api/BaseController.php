e4d279fb204c00e299ddc0a5c647518c9e899067
Oxcode <samuel@tedbree.com>
2025-10-15 23:53:10 +0100

    initial commit

--- app/Http/Controllers/Api/BaseController.php
+++ app/Http/Controllers/Api/BaseController.php
@@ -1,10 +1,57 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller as Controller;
+use Illuminate\Http\JsonResponse;
 
 class BaseController extends Controller
 {
-    //
+     /**
+     * success response method.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    protected function confirmUser($user) :JsonResponse
+    {
+        if (!$user) {
+            return $this->sendError('Validation Error.', ['status' => 'failed', 'message' => 'user not found'], 419);       
+        }
+    }
+    /**
+     * success response method.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function sendResponse($result, $message) :JsonResponse
+    {
+    	$response = [
+            'success' => true,
+            'data'    => $result,
+            'message' => $message,
+        ];
+
+        return response()->json($response, 200);
+    }
+
+    /**
+     * return error response.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function sendError($error, $errorMessages = [], $code = 404) :JsonResponse
+    {
+    	$response = [
+            'success' => false,
+            'message' => $error,
+            'status_code' => $code,
+        ];
+
+        if(!empty($errorMessages)){
+            $response['data'] = $errorMessages;
+        }
+
+        return response()->json($response, $code);
+    }
 }
