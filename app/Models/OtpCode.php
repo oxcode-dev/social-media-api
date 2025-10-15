c61c8f89f6de0406a109085bea99702bc15a1297
Oxcode <samuel@tedbree.com>
2025-10-15 23:59:12 +0100

    initial commit

--- app/Models/OtpCode.php
+++ app/Models/OtpCode.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class OtpCode extends Model
 {
-    //
+    protected $table = 'otp_codes';
+
+    protected $fillable = [
+        'code',
+        'email',
+        'expires_at',
+    ];
 }
