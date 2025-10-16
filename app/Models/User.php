5fc7028e8b5a2b701d8ab50799107eda6fcf140a
Oxcode <samuel@tedbree.com>
2025-10-16 23:28:09 +0100

    initial commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,21 +31,6 @@ class User extends Authenticatable
         'avatar',
     ];
 
-    // 'id' => fake()->uuid(),
-    //         'username' => fake()->userName(),
-    //         'first_name' => fake()->firstName(),
-    //         'last_name' => fake()->lastName(),
-    //         'email' => fake()->unique()->safeEmail(),
-    //         'phone' => fake()->unique()->phoneNumber(),
-    //         'bio' => fake()->realText(),
-    //         'avatar' => fake()->imageUrl(),
-    //         'email_verified_at' => now(),
-    //         'password' => static::$password ??= 'password',
-    //         'remember_token' => Str::random(10),
-    //         'two_factor_secret' => Str::random(10),
-    //         'two_factor_recovery_codes' => Str::random(10),
-    //         'two_factor_confirmed_at' => now(),
-
     /**
      * The attributes that should be hidden for serialization.
      *
