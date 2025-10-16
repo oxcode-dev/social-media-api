98ef1cb8fffeef813bf9066d35647a350b68f779
Oxcode <samuel@tedbree.com>
2025-10-16 23:29:05 +0100

    initial commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'first_name' => 'Test',
+            'last_name' => 'User',
+            'username' => 'test_user',
             'email' => 'test@example.com',
         ]);
     }
