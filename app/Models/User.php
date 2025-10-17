4f25b0f9c65102e4132cc83c8ef53faaad76fab5
Oxcode <samuel@tedbree.com>
2025-10-17 17:12:28 +0100

    elite dashboard merge

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,7 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use App\Notifications\ResetPasswordNotification;
 
 class User extends Authenticatable
 {
@@ -55,5 +56,68 @@ class User extends Authenticatable
             'password' => 'hashed',
             'two_factor_confirmed_at' => 'datetime',
         ];
+    }
+
+    public static function search($query)
+    {
+        $relations = ['products', 'addresses', 'orders'];
+
+        return empty($query) ? static::query()->with($relations)
+            : static::with($relations)
+                ->where('first_name', 'like', '%'.$query.'%')
+                ->where('last_name', 'like', '%'.$query.'%')
+                // ->orWhere('source', 'like', '%'.$query.'%')
+                ->orWhere('email', 'like', '%'.$query.'%');
+    }
+
+
+    public function generatePin($digits = 4): string
+    {
+        $i = 0; //counter
+        $pin = ''; //our default pin is blank.
+
+        while ($i < $digits) {
+            //generate a random number between 0 and 9.
+            $pin .= random_int(0, 9);
+            $i++;
+        }
+
+        return $pin;
+    }
+
+    public function sendNewUserNotification($token = null)
+    {
+        $result = $this->generatePin(5);
+
+        $this['otp'] = $result;
+
+        // $this->notify(new NewUserNotification($this));
+
+        OtpCode::where('email', $this->email)->delete();
+
+        OtpCode::create([
+            'code' => $result,
+            'email' => $this->email,
+            'expires_at' => now()->addMinutes(5),
+        ]);
+
+        return $result;
+    }
+
+    public function sendPasswordResetNotification($token = null)
+    {
+        $result = $this->generatePin(5);
+
+        $this->notify(new ResetPasswordNotification($result));
+
+        OtpCode::where('email', $this->email)->delete();
+
+        OtpCode::create([
+            'code' => $result,
+            'email' => $this->email,
+            'expires_at' => now()->addMinutes(5),
+        ]);
+
+        return $result;
     }
 }
