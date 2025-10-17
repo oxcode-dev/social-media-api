5d06b4e3ab3fea31febeedea065a111e421d43e8
Oxcode <samuel@tedbree.com>
2025-10-17 23:59:00 +0100

    elite dashboard merge

--- app/Models/Follower.php
+++ app/Models/Follower.php
@@ -10,4 +10,27 @@ class Follower extends Model
 {
     /** @use HasFactory<\Database\Factories\FollowerFactory> */
     use HasFactory, HasUuids;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'follower_id',
+        'following_id',
+    ];
+
+     /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'follower_id' => 'uuid',
+            'following_id' => 'uuid',
+        ];
+    }
 }
