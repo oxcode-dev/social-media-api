45e365291f6e1fe4f0d2bba092e676b7612033b9
Oxcode <samuel@tedbree.com>
2025-10-17 23:55:22 +0100

    elite dashboard merge

--- app/Models/Follower.php
+++ app/Models/Follower.php
@@ -2,11 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Follower extends Model
 {
     /** @use HasFactory<\Database\Factories\FollowerFactory> */
-    use HasFactory;
+    use HasFactory, HasUuids;
 }
