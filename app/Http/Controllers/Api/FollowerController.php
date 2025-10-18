2f6ae53921b57044400c342b3fb7cc99f83b2845
Oxcode <samuel@tedbree.com>
2025-10-18 01:16:23 +0100

    elite dashboard merge

--- app/Http/Controllers/Api/FollowerController.php
+++ app/Http/Controllers/Api/FollowerController.php
@@ -3,13 +3,59 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\API\BaseController;
-
+use App\Models\Follower;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class FollowerController extends BaseController
 {
     public function store(Request $request, $id)
     {
-        return [$request->all(), $id];
+        $authUser = $request->user();
+
+        if (!$authUser) {
+            return $this->sendError('Validation Error.', ['status' => 'failed', 'message' => 'user not found'], 419);       
+        }
+
+        $user = User::find($id);
+        if(!$user) {
+            return $this->sendError('Error Occurred.', ['status' => 'failed', 'message' => 'User not found'], 419);       
+        }
+
+        if(!Follower::where('following_id', $id)->where('follower_id', $authUser->id)->first()) {
+            $following = new Follower();
+            $following->following_id = $id;
+            $following->follower_id = $authUser->id;
+
+            $following->save();
+            
+            return $this->sendResponse(['User Followed Successfully'], 'User Followed Successfully.');
+        }
+
+        return $this->sendResponse([], 'You are already following this user!!!.');
+
+    }
+
+    public function destroy(Request $request, $id)
+    {
+        return [$request->user(), $id];
+        $authUser = $request->user();
+
+        if (!$authUser) {
+            return $this->sendError('Validation Error.', ['status' => 'failed', 'message' => 'user not found'], 419);       
+        }
+
+        $user = User::find($id);
+        if(!$user) {
+            return $this->sendError('Error Occurred.', ['status' => 'failed', 'message' => 'User not found'], 419);       
+        }
+
+        if($following = Follower::where('following_id', $id)->where('follower_id', $authUser->id)->first()) {
+            $following->delete();
+            
+            return $this->sendResponse(['User Unfollowed Successfully'], 'User Unfollowed Successfully.');
+        }
+
+        return $this->sendResponse([], 'You are not following this user!!!.');
     }
 }
